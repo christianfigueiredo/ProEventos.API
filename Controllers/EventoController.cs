cc50627661127fffe9882faaac9e3c2cf47acc66
developer.figueiredo <developer.figueiredo@gmail.com>
2025-07-24 21:11:28 -0300

    alterado var evento

--- Controllers/EventoController.cs
+++ Controllers/EventoController.cs
@@ -7,24 +7,37 @@ namespace ProEvento.API.Controllers;
 [Route("[controller]")]
 public class EventoController : ControllerBase
 {
+    public IEnumerable<Evento> evento = new Evento[]{
+            new Evento() {
+                EventoId = 1,
+                Local = "São Paulo",
+                Tema = "Angular e .NET 6",
+                DataEvento = "01/05/2023",
+                QtdePessoas = 250,
+                Lote = "1º Lote",
+                ImagemURL = "foto.png"
+            },
+            new Evento() {
+                EventoId = 2,
+                Local = "Rio de Janeiro",
+                Tema = "React e .NET 6",
+                DataEvento = "02/06/2023",
+                QtdePessoas = 300,
+                Lote = "2º Lote",
+                ImagemURL = "foto2.png"
+            }
+
+        };
     public EventoController()
     {
 
     }
 
     [HttpGet]
-    public Evento Get()
+    public IEnumerable<Evento> Get()
     {
-        return new Evento()
-        {
-            EventoId = 1,
-            Tema = "Angular e .NET 6",
-            Local = "São Paulo",
-            Lote = "1º Lote",
-            QtdePessoas = 250,
-            DataEvento = "01/05/2023",
-            
-        };
+        return evento;
+       
     }
 
     [HttpPost]
