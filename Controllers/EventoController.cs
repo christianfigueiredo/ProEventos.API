c4d41c6d562fead8815ae043e86a8bcc06f5ae0e
developer.figueiredo <developer.figueiredo@gmail.com>
2025-07-24 21:02:18 -0300

    adicionando eventos

--- Controllers/EventoController.cs
+++ Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
 
 namespace ProEvento.API.Controllers;
 
@@ -12,14 +13,35 @@ public class EventoController : ControllerBase
     }
 
     [HttpGet]
-    public string Get()
+    public Evento Get()
     {
-        return "Certin";
+        return new Evento()
+        {
+            EventoId = 1,
+            Tema = "Angular e .NET 6",
+            Local = "São Paulo",
+            Lote = "1º Lote",
+            QtdePessoas = 250,
+            DataEvento = "01/05/2023",
+            
+        };
     }
 
     [HttpPost]
     public string Post()
     {
         return "Post";
+    }
+
+    [HttpPut("{id:int}")]
+    public string Put(int id)
+    {
+        return $"Exemploe de id {id} recebido";
+    }
+
+    [HttpDelete("{id:int}")]
+    public string Delete(int id)
+    {    
+        return $"Exemploe de id {id} recebido para exclusão";
     }
 }
